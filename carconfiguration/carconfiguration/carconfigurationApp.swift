a58526fe288efba743722eacaa9ceb7206265e36
jordyntchorere <jordyntchorere@gmail.com>
2023-11-10 14:56:50 +0100

    projet vide

---
+++ carconfiguration/carconfiguration/carconfigurationApp.swift
@@ -0,0 +1,17 @@
+//
+//  carconfigurationApp.swift
+//  carconfiguration
+//
+//  Created by NTCHORERE on 10/11/2023.
+//
+
+import SwiftUI
+
+@main
+struct carconfigurationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
